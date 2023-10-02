307179570c95984ea14f4ca008d223671ad7a086
lsyversen <syversenliam@gmail.com>
2023-10-01 21:42:44 -0400

    updates

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -17,7 +17,7 @@ const NavBar = () => {
         <header>
             <h3>AI Image Gen</h3>
             <div className="menu">
-            <Link className='link' to="/">Showcase</Link>
+            <Link className='link' to="/">Gallery</Link>
             {user && <Link className='link' to={"/generate"}>Generate</Link>}
             {user? <div className='link'><div className='d-flex'><img className='logo' src={user.photoURL} alt={user.displayName} />
             <button onClick={logOut}>Sign Out</button>
